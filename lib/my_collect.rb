0577891fa3a7bb0ce3294fb6c06ac1c7e61b058f
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 18:34:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,12 +4,10 @@ def my_collect(array)
   if block_given?
   x = 0
   new_collection = []
-
     while x < array.length
       new_collection << yield(array[x])
       x += 1
     end
-
   new_collection
 end
 
