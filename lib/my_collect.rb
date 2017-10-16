517a83b8eb3640f2cc851679bba155d1a3851b9d
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 18:21:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,13 +4,13 @@ def my_collect(array)
   new_collection = []
 
     while x < array.length
-      new_collection << yield([x])
+      new_collection << yield(array)
       x += 1
     end
 
   new_collection
 end
 
-my_collect([x]) do |each|
+my_collect(array) do |each|
   each.upcase
 end
