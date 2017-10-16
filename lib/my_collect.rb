b44247594826e8734c54b74e1c37991e4266d870
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 18:14:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,10 @@
 def my_collect(array)
   x = 0
   new_collection = []
-  while x < array.length
-    new_collection << yield(array)
-    x += 1
-  end
+
+    while x < array.length
+      new_collection << yield(array)
+      x += 1
+    end
 
 end
